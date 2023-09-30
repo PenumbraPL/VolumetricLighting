cc6a1d57ac344d5b74ebf57156fa011739b0de2f
Jan Bizon <janek.bizon@gmail.com>
2023-09-30 14:52:57 +0200

    Konfiguracja wstępna - pobranie niezbędnych bibliotek oraz inicjalizacja repozytorium.

---
+++ VolumetricLighting.cpp
@@ -0,0 +1,15 @@
+// VolumetricLighting.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
