98799418fc63c30b7c70c0f14019037208af2c7d
Jan Bizon <janek.bizon@gmail.com>
2023-09-30 16:57:28 +0200

    Stworzenie prostej aplikacji bazowej do dalszego rozwoju.

--- VolumetricLighting.cpp
+++ VolumetricLighting.cpp
@@ -1,5 +1,6 @@
 // VolumetricLighting.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#define GLEW_STATIC
 
 #include <iostream>
 #include "GL/glew.h"
@@ -9,7 +10,137 @@
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 
-int main()
+
+struct WindowInfo {
+    int width;
+    int height;
+    const char* title;
+};
+
+
+int main(void)
 {
     std::cout << "Hello World!\n";
+
+    GLFWwindow* window;
+    WindowInfo windowConfig = {
+        1280,
+        960,
+        "Simple Triangles"
+    };
+    
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        std::cout << "GLEW - init unsuccessful\n\t" << "Error:" << glewGetErrorString(err);
+    }
+
+    if (!glfwInit()) {
+        std::cout << "GLFW - init unsuccessful\n";
+        return -1;
+    }
+
+    window = glfwCreateWindow(windowConfig.width, windowConfig.height, windowConfig.title, NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        std::cout << "GLFW - terminated\n";
+        std::cout << "GLFW - window init unsuccessful\n";
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    const char* glsl_version = "#version 150"; // TO DO
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+
+    glm::vec2 p1(-0.5, 0.5);
+    glm::vec2 p2(0.5, 0.5);
+    glm::vec2 p3(0, -0.5);
+
+    std::cout << "---- main loop ----\n";
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glBegin(GL_TRIANGLES);
+        glVertex2f (-0.5, 0.5);
+        glVertex2f (0.5, 0.5);
+        glVertex2f (0, -0.5);
+        glEnd();
+
+
+        // ImGui
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        bool show_demo_window = true;
+        bool show_another_window = false;
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwTerminate();
+    std::cout << "GLFW - terminated\n";
+    std::cout << "---- exit successful -----\n";
+    return 0;
 }
