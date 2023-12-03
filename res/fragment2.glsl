101163fc78ec25aca5b5682c285f26c8712368d6
Jan Bizon <janek.bizon@gmail.com>
2023-12-03 21:27:41 +0100

    Pierwsza próba wyświetlenia tekstury - Nieudana.

--- res/fragment2.glsl
+++ res/fragment2.glsl
@@ -1,12 +1,16 @@
 #version 450 core
 
+layout (binding = 0) uniform sampler2D tex;
+
 in VS_OUT {
 vec3 _normal;
 vec3 _color;
+vec2 _texCoor;
 }fs_in;
+
 out vec4 color;
 
 void main()
 {
-    color = vec4(fs_in._normal, 1.0);
+    color = vec4(fs_in._texCoor, 1.0, 1.0);
 }
